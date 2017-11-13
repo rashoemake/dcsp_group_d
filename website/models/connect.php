7873a91a045c92f9344552e932f1b761d52094a6
Will Carroll <will.carroll7@gmail.com>
2017-11-13 01:00:29 +0000

    fixes

--- website/models/connect.php
+++ website/models/connect.php
@@ -4,4 +4,9 @@
     $database = "netid"
     $username = "netid"
     $password = "password"
+
+    $conn = new mysqli($hostname, $username, $password, $database);
+    if ($conn->connect_error) {
+        die($conn->connect_error);
+    }
 ?>
