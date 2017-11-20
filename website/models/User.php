8da8f6508c39ffaf0d2d0dd6c988d816357918ab
Will Carroll <will.carroll7@gmail.com>
2017-11-19 19:27:06 -0600

    fixed errors in User.php, tested functionality

--- website/models/User.php
+++ website/models/User.php
@@ -34,20 +34,18 @@ class User {
 		require("connect.php");
         
         // Query for the ID
-        if (!($stmt = $conn->prepare("SELECT * FROM `$tablename` WHERE emailAddress=?"))) {
+        if ($stmt = $conn->prepare("SELECT * FROM `users` WHERE emailAddress=?")) {
             header('HTTP/1.1 500 Internal Server Error');	
         }
-
-        if (!($stmt->bind_param("s", $email))) {
-            header('HTTP/1.1 500 Internal Server Error');
-        }
+        $stmt->bind_param("s", $email);
 
         if (!($stmt->execute())) {
             header('HTTP/1.1 500 Internal Server Error');
         }
 
         $row = $stmt->get_result()->fetch_assoc();
-        $return_value = User::from_assoc($row);
+        $return_value = new User();
+        $return_value->from_assoc($row);
         return $return_value;
     }
 
@@ -62,65 +60,16 @@ class User {
 		require("connect.php");
         
         // Query for the ID
-        if (!($stmt = $conn->prepare("INSERT INTO `$tablename` (emailAddress, name, passwordHash) VALUES (?, ?, ?)"))) {
+        if ($stmt = $conn->prepare("INSERT INTO `users` (emailAddress, name, passwordHash) VALUES (?, ?, ?)")) {
             header('HTTP/1.1 500 Internal Server Error');	
         }
+        $stmt->bind_param("sss", $new_user->get_email_address(), $new_user->get_name(), $new_user->get_password_hash());
 
-        if (!($stmt->bind_param("sss", $new_user->get_email_address(), $new_user->get_name(), $new_user->get_password_hash()))) {
+        if (!$stmt->execute()) {
             header('HTTP/1.1 500 Internal Server Error');
         }
 
-        if (!($stmt->execute())) {
-            header('HTTP/1.1 500 Internal Server Error');
-        }
-
-        $return $conn->insert_id;
-    }
-
-    public static function from_assoc($assoc) {
-        if (isset($assoc["id"])) {
-            $id = $assoc["id"];
-        }
-
-        if (isset($assoc["name"])) {
-            $name = $assoc["name"];
-        }
-
-        if (isset($assoc["emailAddress"])) {
-            $email_address = $assoc["emailAddress"];
-        }
-
-        if (isset($assoc["passwordSalt"])) {
-            $password_salt = $assoc["passwordSalt"];
-        }
-
-        if (isset($assoc["passwordHash"])) {
-            $password_hash = $assoc["passwordHash"];
-        }
-
-        if (isset($assoc["avgRating"])) {
-            $avg_rating = $assoc["avgRating"];
-        }
-
-        if (isset($assoc["numRatings"])) {
-            $num_ratings = $assoc["numRatings"];
-        }
-
-        if (isset($assoc["bio"])) {
-            $biography = $assoc["bio"];
-        }
-
-        if (isset($assoc["disabled"])) {
-            $disabled = $assoc["disabled"];
-        }
-
-        if (isset($assoc["modifiedDate"])) {
-            $modifiedDate = $assoc["modifiedDate"];
-        }
-
-        if (isset($assoc["university_id"])) {
-            $university_id = $assoc["university_id"];
-        }
+        return $conn->insert_id;
     }
 
 
@@ -215,8 +164,54 @@ class User {
         return $this->modifiedDate;
     }
 
+    private function set_modified_date($modifiedDate) {
+        $this->modifiedDate = $modifiedDate;
+    }
+
 
     // Instance Methods
+    public function from_assoc($assoc) {
+        if (isset($assoc["id"])) {
+            $this->set_id($assoc["id"]);
+        }
+
+        if (isset($assoc["name"])) {
+            $this->set_name($assoc["name"]);
+        }
+
+        if (isset($assoc["emailAddress"])) {
+            $this->set_email_address($assoc["emailAddress"]);
+        }
+
+        if (isset($assoc["passwordHash"])) {
+            $this->set_password_hash($assoc["passwordHash"]);
+        }
+
+        if (isset($assoc["avgRating"])) {
+            $this->set_avg_rating($assoc["avgRating"]);
+        }
+
+        if (isset($assoc["numRatings"])) {
+            $this->set_num_ratings($assoc["numRatings"]);
+        }
+
+        if (isset($assoc["bio"])) {
+            $this->set_biography($assoc["bio"]);
+        }
+
+        if (isset($assoc["disabled"])) {
+            $this->set_disabled($assoc["disabled"]);
+        }
+
+        if (isset($assoc["modifiedDate"])) {
+            $this->set_modified_date($assoc["modifiedDate"]);
+        }
+
+        if (isset($assoc["university_id"])) {
+            $this->set_university_id($assoc["university_id"]);
+        }
+    }
+    
     public function add_rating() {
         // TODO
     }
