acdda87f4ff254d87053386efe9ede4580f78c09
Ryan Shoemake <ras603@msstate.edu>
2017-11-23 16:37:44 -0600

    Moved navbar code into its own file (website\php_scripts\navbar.php)

---
+++ website/php_scripts/navbar.php
@@ -0,0 +1,14 @@
+    <!-- Begin Navbar -->
+		<nav class="navbar navbar-inverse">
+      <div class="container">
+        <ul class="nav navbar-nav">
+          <li class="navbar-padding"><a href="index.php"><span class="glyphicon glyphicon-home glyph-padding"></span>Home</a></li>
+          <li class="navbar-padding"><a href="about.php">About</a></li>
+          <li class="navbar-padding"><a href="#">Contact</a></li>
+        </ul>
+        <ul class="nav navbar-nav pull-right">
+          <li><a href="login.php"><span class="glyphicon glyphicon-log-in glyph-padding"></span>Login</a></li>
+          <li style="padding-right: 15px;"><a href="#"><span class="glyphicon glyphicon-user glyph-padding"></span>Sign Up</a></li>          
+        </ul>
+      </div>
+    </nav>
