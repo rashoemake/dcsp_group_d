b3bdbcd0a7ecf8d826a2defcabea5f2814b57bf5
Ryan Shoemake <ras603@msstate.edu>
2017-11-23 18:15:41 -0600

    Setup navbar to change based on login status

--- website/php_scripts/navbar.php
+++ website/php_scripts/navbar.php
@@ -1,4 +1,14 @@
-    <!-- Begin Navbar -->
+<?php
+    if(isset($_SESSION)) {
+        $context='<li style="padding-right: 15px;"><a href="logout.php"><span class="glyphicon glyphicon-log-out glyph-padding"></span>Logout</a></li>';
+
+    } else {
+        $context='<li><a href="login.php"><span class="glyphicon glyphicon-log-in glyph-padding"></span>Login</a></li>'."\n";
+        $context.='<li style="padding-right: 15px;"><a href="sign-up.php"><span class="glyphicon glyphicon-user glyph-padding"></span>Sign Up</a></li>';
+    }
+?>
+
+<!-- Begin Navbar -->
 		<nav class="navbar navbar-inverse">
       <div class="container">
         <ul class="nav navbar-nav">
@@ -7,8 +17,7 @@
           <li class="navbar-padding"><a href="#">Contact</a></li>
         </ul>
         <ul class="nav navbar-nav pull-right">
-          <li><a href="login.php"><span class="glyphicon glyphicon-log-in glyph-padding"></span>Login</a></li>
-          <li style="padding-right: 15px;"><a href="#"><span class="glyphicon glyphicon-user glyph-padding"></span>Sign Up</a></li>          
+          <?php echo $context ?>      
         </ul>
       </div>
     </nav>
